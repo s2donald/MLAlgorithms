55c9c14e385a9b2ca1ba27041637a2c48af3e333
s2donald <s2donald@uwaterloo.ca>
2025-12-05 23:26:56 -0500

    Added KNN classifier algorithm

---
+++ src/knn_classifier.py
@@ -0,0 +1,63 @@
+import numpy as np
+from collections import Counter
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1-x2)**2))
+
+
+class KNN:
+    def __init(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        predictions = [self.helper(x) for x in X]
+        return predictions
+
+    def helper(self, x):
+        #compute the distance
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+
+        # Get the closest k
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+
+        # Majority Vote
+        most_common = Counter(k_nearest_labels).most_common()
+        return most_common[0][0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
